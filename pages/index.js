71d8ba960a0ac898fa8a19acdb16122079a3b794
danielhui <danielhui@yohohongkong.com>
2021-10-21 07:11:47 +0000

    test7

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,24 @@
-import Image from "next/image";
+import MyImage from "../components/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 export default function Home() {
-  let loadTime = Date.now();
-  const [mountImage, setMountImage] = useState(false);
+	let loadTime = Date.now();
+	const [mountImage, setMountImage] = useState(false);
 
-  return (
-    <div className={styles.container}>
-	<button onClick={()=>{setMountImage(true);}}>click to load image</button>
-      {mountImage && (
-        <Image
-          width={1395}
-          height={947}
-          blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="}
-          placeholder="blur"
-          src={"/mountains.jpg"}
-          onLoadingComplete={() => {
-            console.log("test", Date.now() - loadTime);
-          }}
-        />
-      )}
-    </div>
-  );
+	console.log('Home is rendered')
+	return (
+		<div className={styles.container}>
+			<button
+				onClick={() => {
+					setMountImage(true);
+				}}
+			>
+				click to load image
+			</button>
+			{mountImage && (
+				<MyImage/>
+			)}
+		</div>
+	);
 }
